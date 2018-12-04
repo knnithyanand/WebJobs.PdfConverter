ea73eaaeae0ef140505307538412b1b4a8421f89
Nithyanand K N <knnithyanand@hotmail.com>
2018-12-04 16:54:05 +0800

    Initial Commit

---
+++ src/Knnithyanand.Demo.WebJobs.PdfConverter/Functions.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Knnithyanand.Demo.WebJobs.PdfConverter
+{
+    public class Functions
+    {
+        private static int desired_x_dpi = 300;
+        private static int desired_y_dpi = 300;
+
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessPdfFiles(
+            [QueueTrigger("workitems")] string pdfFilename,
+            [Blob("pdf-files/{queueTrigger}.pdf", FileAccess.Read)] Stream pdfBlob,
+            IBinder binder,
+            ILogger logger)
+        {
+            logger.LogInformation($"Start Processing: {pdfFilename}");
+
+            GhostscriptVersionInfo gvi = new GhostscriptVersionInfo(@"gsdll32.dll");
+            using (GhostscriptRasterizer _rasterizer = new GhostscriptRasterizer())
+            {
+                logger.LogInformation($"Open: {pdfFilename}");
+                _rasterizer.Open(pdfBlob, gvi, true);
+                for (int pageNumber = 1; pageNumber <= _rasterizer.PageCount; pageNumber++)
+                {
+                    string pngFilename = $"{pdfFilename}-page-{pageNumber.ToString()}.png".ToLower();
+                    logger.LogInformation($"pageFilePath: {pngFilename}");
+
+                    BlobAttribute blobAttribute = new BlobAttribute($"png-files/{pngFilename}");
+                    CloudBlockBlob pngBlob = binder.Bind<CloudBlockBlob>(blobAttribute);
+
+                    logger.LogInformation($"GetPage: {pageNumber}");
+                    Image img = _rasterizer.GetPage(desired_x_dpi, desired_y_dpi, pageNumber);
+                    logger.LogInformation($"Save: {pngFilename}");
+
+                    MemoryStream ms = new MemoryStream();                    
+                    img.Save(ms, ImageFormat.Png);
+                    ms.Flush();
+                    ms.Position = 0;
+                    pngBlob.UploadFromStream(ms);
+                }
+            }
+
+            logger.LogInformation($"Finish Processing: {pdfFilename}");
+        }
+    }
+}
---
+++ src/Knnithyanand.Demo.WebJobs.PdfConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Configuration;
+
+namespace Knnithyanand.Demo.WebJobs.PdfConverter
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var loggerFactory = new LoggerFactory();
+            var instrumentationKey = ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            config.DashboardConnectionString = "";
+            config.LoggerFactory = loggerFactory
+                .AddApplicationInsights(instrumentationKey, null)
+                .AddConsole();
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
